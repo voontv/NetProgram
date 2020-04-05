25290a8802f6f4e842844bf7f02ab5e15c788176
voontv <voontv@gmail.com>
2020-04-05 12:07:51 +0700

    completed exercise01

---
+++ NETPROGRAM/BasicExercise/Exercise01.cs
@@ -0,0 +1,37 @@
+﻿
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise01
+    {
+
+        public int UCLN(int a,int b)
+        {
+            if (a * b == 0) return a + b;
+
+            var numA = (a > b) ? b : a;
+
+            var numB = (a > b) ? a : b;
+
+            while(numA != numB)
+            {
+                if(numA > numB)
+                {
+                    numA -= numB;
+                }
+                else
+                {
+                    numB -= numA;
+                }                
+            }
+
+            return numA;
+        }
+
+        public long BCNN(int a, int b)
+        {
+            if (a * b == 0) return 0;
+
+            return a *b / UCLN(a, b);
+        }
+    }
+}

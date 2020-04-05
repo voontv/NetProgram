88ff571fe0fedaf45cf03772b408bdec29ec0518
voontv <voontv@gmail.com>
2020-04-05 17:38:13 +0700

    add Exercise02

---
+++ NETPROGRAM/BasicExercise/Exercise02.cs
@@ -0,0 +1,21 @@
+﻿namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise02
+    {
+
+        public int sumNumberInt(int a)
+        {
+
+            var number = a;
+            int sum = 0;
+
+            do
+            {
+                sum += number % 10;
+                number = number / 10;
+            } while (number != 0);
+
+                return sum;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise02Test.cs
@@ -0,0 +1,23 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise02Test
+    {
+        
+        [TestMethod]
+        public void sumNumberIntTest()
+        {
+
+            var test = new Exercise02();
+
+            Assert.AreEqual(test.sumNumberInt(129), 12);
+            Assert.AreEqual(test.sumNumberInt(500), 5);
+            Assert.AreEqual(test.sumNumberInt(9), 9);
+            Assert.AreNotEqual(test.sumNumberInt(91), 9);
+            
+        }
+    }
+}

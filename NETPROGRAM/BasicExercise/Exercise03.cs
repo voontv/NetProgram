70bd238a66b45d969a5688024ca3597ce8675f2c
voontv <voontv@gmail.com>
2020-04-06 08:44:36 +0700

    add exercise03

---
+++ NETPROGRAM/BasicExercise/Exercise03.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise03
+    {
+
+        public bool isPrime(int n)
+        {
+
+            if(n < 2)
+            {
+                return false;
+            }
+
+            for(int i=2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public String showMultiPrime(int n)
+        {
+
+            String stringPrime = null;
+            var i = 2;
+
+            if(n <= i)
+            {
+                return n.ToString();
+            }
+
+            do
+            {
+                if (n % i == 0 && isPrime(i))
+                {
+                    if (stringPrime != null)
+                    {
+                        stringPrime += " * " + i;
+                    }
+                    else
+                    {
+                        stringPrime += i;
+                    }
+                    n /= i;
+                }
+                else
+                {
+                    i++;
+                }
+            } while (i <= n);
+
+            return stringPrime;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise03Test.cs
@@ -0,0 +1,39 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+
+    [TestClass]
+    public class Exercise03Test
+    {
+
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            var test = new Exercise03();
+
+            Assert.AreEqual(test.isPrime(1), false);
+            Assert.AreEqual(test.isPrime(0), false);
+            Assert.AreEqual(test.isPrime(3), true);
+            Assert.AreEqual(test.isPrime(5), true);
+            Assert.AreEqual(test.isPrime(9), false);
+            Assert.AreEqual(test.isPrime(7), true);
+        }
+
+        [TestMethod]
+        public void testShowMultiPrime()
+        {
+
+            var test = new Exercise03();
+
+            Assert.AreEqual(test.showMultiPrime(9), "3 * 3");
+            Assert.AreEqual(test.showMultiPrime(12), "2 * 2 * 3");
+            Assert.AreEqual(test.showMultiPrime(7), "7");
+            Assert.AreEqual(test.showMultiPrime(33), "3 * 11");
+            Assert.AreEqual(test.showMultiPrime(1), "1");
+            Assert.AreEqual(test.showMultiPrime(0), "0");
+        }
+    }
+
+}

79ad43fafbb20db9dd6eba5d0085afc6007c524d
voontv <voontv@gmail.com>
2020-04-06 21:39:14 +0700

    add Exercise07

---
+++ NETPROGRAM/BasicExercise/Exercise07.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise07
+    {
+
+        public long Factorial(int n)
+        {
+            if(n < 1)
+            {
+                return 1;
+            }
+            var result = 1;
+
+            for(int i =1; i <=n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise07Test.cs
@@ -0,0 +1,20 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise07Test
+    {
+
+        [TestMethod]
+        public void TestFactorial()
+        {
+            var test = new Exercise07();
+
+            Assert.AreEqual(test.Factorial(3), 6);
+            Assert.AreEqual(test.Factorial(0), 1);
+            Assert.AreEqual(test.Factorial(4), 24);
+        }
+    }
+}

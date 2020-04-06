86623305b8d00d336949ab55d05d7d07853d29e6
voontv <voontv@gmail.com>
2020-04-06 21:32:12 +0700

    add Exercise06

---
+++ NETPROGRAM/BasicExercise/Exercise06.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise06
+    {
+
+        public int SumWithN(int n)
+        {
+            if(n < 1)
+            {
+                throw new Exception("Wrong input");
+            }
+
+            var sum = 0;
+            
+            for(int i =1; i<=n; i++)
+            {
+                sum += i * (i + 1);
+            }
+
+            return sum;
+        }
+    }
+}

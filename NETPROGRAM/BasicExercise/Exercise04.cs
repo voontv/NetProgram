8eaf6bf497842a08f8e5edb97d1ffc046c1931b3
voontv <voontv@gmail.com>
2020-04-06 09:24:19 +0700

    add exercise04

---
+++ NETPROGRAM/BasicExercise/Exercise04.cs
@@ -0,0 +1,48 @@
+﻿namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise04
+    {
+
+        public int FiboInPositionN(int n)
+        {
+
+            if (n <= 2 && n > 0)
+                return 1;
+
+            return FiboInPositionN(n - 1) + FiboInPositionN(n - 2);
+        }
+
+        public string showRangeFibo(int n)
+        {
+            string rangeFibo = null;
+
+            if(n < 1)
+            {
+                return "Can not find Fibo number same request";
+            }
+
+            for(int i = 0; i<= n; i++)
+            {
+                if (FiboInPositionN(i+1) <= n)
+                {
+
+                    if (rangeFibo != null)
+                    {
+                        rangeFibo += " " + FiboInPositionN(i + 1);
+                    }
+                    else
+                    {
+                        rangeFibo += FiboInPositionN(i + 1);
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return rangeFibo;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise04Test.cs
@@ -0,0 +1,35 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise04Test
+    {
+
+        [TestMethod]
+        public void testFiboInPositionN()
+        {
+
+            var test = new Exercise04();
+
+            Assert.AreEqual(test.FiboInPositionN(1), 1);
+            Assert.AreEqual(test.FiboInPositionN(2), 1);
+            Assert.AreEqual(test.FiboInPositionN(5), 5);
+            Assert.AreEqual(test.FiboInPositionN(6), 8);
+        }
+
+        [TestMethod]
+        public void testshowRangeFibo()
+        {
+
+            var test = new Exercise04();
+
+            Assert.AreEqual(test.showRangeFibo(0), "Can not find Fibo number same request");
+            Assert.AreEqual(test.showRangeFibo(2), "1 1 2");
+            Assert.AreEqual(test.showRangeFibo(4), "1 1 2 3");
+            Assert.AreEqual(test.showRangeFibo(5), "1 1 2 3 5");
+            Assert.AreEqual(test.showRangeFibo(6), "1 1 2 3 5");
+        }
+    }
+}

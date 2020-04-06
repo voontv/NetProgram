4b3b027c3637a6903fce1c24b0b4537546a5bdec
voontv <voontv@gmail.com>
2020-04-06 10:31:59 +0700

    add exercise05

---
+++ NETPROGRAM/BasicExercise/Exercise05.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise05
+    {
+   
+        public bool checkNumberRequest(int n)
+        {
+            int length = n.ToString().Length;
+            int result = 0;
+            int number = n;
+
+            do
+            {
+
+                result += (int) Math.Pow(number % 10, length);
+                number /= 10;
+
+            } while (number > 0);
+
+
+            return (result == n);
+        }
+
+        public List<int> showListNumber(int n)
+        {
+            List<int> numberList = new List<int>();
+
+            for(int i = 100; i <= n; i++)
+            {
+                if(checkNumberRequest(i))
+                {
+                    numberList.Add(i);
+                }
+            }
+
+            return numberList;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise05Test.cs
@@ -0,0 +1,31 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+
+    [TestClass]
+    public class Exercise05Test
+    {
+
+        [TestMethod]
+        public void testCheckNumberRequest()
+        {
+            var test = new Exercise05();
+
+            Assert.AreEqual(test.checkNumberRequest(153), true);
+        }
+
+        [TestMethod]
+        public void testShowListNumber()
+        {
+            var test = new Exercise05();
+            var listTest = new List<int>() { 153 };
+
+            Assert.AreEqual(test.showListNumber(154).IndexOf(0), listTest.IndexOf(0));
+            Assert.AreEqual(test.showListNumber(154).Count, listTest.Count);
+            Assert.AreEqual(test.showListNumber(10000).Count, 7);
+        }
+    }
+}

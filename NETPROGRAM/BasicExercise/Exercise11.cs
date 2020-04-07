04a39d5d958d24da91e411043afdffd50436e689
voontv <voontv@gmail.com>
2020-04-07 10:22:19 +0700

    Add exercise11

---
+++ NETPROGRAM/BasicExercise/Exercise11.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise11
+    {
+
+        public int BinaryNumberReverse(int n)
+        {
+            var number = 0;
+            var listNumber = new List<int>();
+
+            if(n == 0)
+            {
+                return 0;
+            }
+
+            while(n > 0)
+            {
+                listNumber.Add(n % 2);
+                n /= 2;
+            }
+
+            for(int i = 0; i<listNumber.Count; i++)
+            {
+                number += listNumber[i] * (int) System.Math.Pow(2, listNumber.Count - i - 1);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise11Test.cs
@@ -0,0 +1,21 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+
+    [TestClass]
+    public class Exercise11Test
+    {
+
+        [TestMethod]
+        public void TestBinaryNumber()
+        {
+            var test = new Exercise11();
+
+            Assert.AreEqual(test.BinaryNumberReverse(8), 1);
+            Assert.AreEqual(test.BinaryNumberReverse(0), 0);
+            Assert.AreEqual(test.BinaryNumberReverse(7), 7);
+        }
+    }
+}

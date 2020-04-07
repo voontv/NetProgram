2f098c719357480ea08376d56036c58447642544
voontv <voontv@gmail.com>
2020-04-07 10:42:01 +0700

    Add Exercise12

---
+++ NETPROGRAM/BasicExercise/Exercise12.cs
@@ -0,0 +1,29 @@
+﻿
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise12
+    {
+
+        public bool SymmetryNumber(int n)
+        {
+            var length = n.ToString().Length;
+            var sum = 0;
+            var i = 1;
+            var temp = n;
+
+            if(n == 0)
+            {
+                return true;
+            }
+
+            while(n != 0)
+            {
+                sum += (n % 10) * (int) System.Math.Pow(10, length - i);
+                n /= 10;
+                i++;
+            }
+
+            return (sum == temp);
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise12Test.cs
@@ -0,0 +1,22 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise12Test
+    {
+
+        [TestMethod]
+        public void TestSymmetryNumber()
+        {
+            var test = new Exercise12();
+
+            Assert.AreEqual(test.SymmetryNumber(0), true);
+            Assert.AreEqual(test.SymmetryNumber(8), true);
+            Assert.AreEqual(test.SymmetryNumber(828), true);
+            Assert.AreEqual(test.SymmetryNumber(129), false);
+            Assert.AreEqual(test.SymmetryNumber(1234321), true);
+        }
+    }
+}

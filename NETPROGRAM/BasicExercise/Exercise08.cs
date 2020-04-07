8a908c125583b6a4f9ba0310b158c8c54196329c
voontv <voontv@gmail.com>
2020-04-07 09:29:44 +0700

    Add exercise08

---
+++ NETPROGRAM/BasicExercise/Exercise08.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise08
+    {
+        const double E = 0.0001;
+
+        public double PI()
+        {
+            var result = 0.0;
+            var i = 0;
+
+            while (1.0/(2 * i + 1) > E)
+            {
+                    
+                result += System.Math.Pow(-1, i) * (1.0 / (2 * i + 1));                         
+                i++;
+
+            }
+
+            return System.Math.Round(4.0 * result,5);
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise08Test.cs
@@ -0,0 +1,19 @@
+﻿using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise08Test
+    {
+
+        [TestMethod]
+        public void TestPI()
+        {
+            var test = new Exercise08();
+
+            Assert.AreEqual(test.PI(), 3.14139);
+        }
+
+    }
+}

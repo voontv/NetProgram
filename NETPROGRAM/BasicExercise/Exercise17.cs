83083dd1916c54ba5111ff9e7660d30d48b8f95e
voontv <voontv@gmail.com>
2020-04-09 16:00:17 +0700

    add Exercise17

---
+++ NETPROGRAM/BasicExercise/Exercise17.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise17
+    {
+        public const int MaxValue = 2147483647;
+
+        public bool IsPrime(int n)
+        {
+
+            if(n < 2)
+            {
+                return false;
+            }
+
+            for(int i = 2; i < n/i; i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsSymmetric(int n)
+        {
+            var number = n;
+            var sum = 0;
+
+            while(number > 0)
+            {
+                sum = sum * 10 + number % 10;
+                number /= 10;
+            }
+
+            return n == sum;
+        }
+
+        public int FindNumbers(int n)
+        {
+            var m = n;
+
+            while( m>= n)
+            {
+                if(IsPrime(m) && IsSymmetric(m))
+                {
+                    break;
+                }
+                else
+                {
+                    m++;
+                }
+            }
+            return m;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise17Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise17Test
+    {
+
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            var test = new Exercise17();
+
+            Assert.AreEqual(test.IsPrime(53), true);
+            Assert.AreEqual(test.IsPrime(13), true);
+            Assert.AreEqual(test.IsPrime(17), true);
+            Assert.AreEqual(test.IsPrime(7), true);
+            Assert.AreEqual(test.IsPrime(1), false);
+            Assert.AreEqual(test.IsPrime(0), false);
+        }
+
+        [TestMethod]
+        public void TestIsSymmetric()
+        {
+            var test = new Exercise17();
+
+            Assert.AreEqual(test.IsSymmetric(9), true);
+            Assert.AreEqual(test.IsSymmetric(1234321), true);
+            Assert.AreEqual(test.IsSymmetric(121), true);
+            Assert.AreEqual(test.IsSymmetric(1231), false);
+            Assert.AreEqual(test.IsSymmetric(234435453), false);
+            Assert.AreEqual(test.IsSymmetric(87874), false);
+        }
+
+        [TestMethod]
+        public void TestFindNumbers()
+        {
+            var test = new Exercise17();
+
+            Assert.AreEqual(test.FindNumbers(100), 101);
+            Assert.AreEqual(test.FindNumbers(145), 151);
+            Assert.AreEqual(test.FindNumbers(2324), 10201);
+            Assert.AreEqual(test.FindNumbers(455454), 1003001);
+        }
+    }
+}

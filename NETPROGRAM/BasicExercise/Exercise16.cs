884714f67840921dcbb0e98b93a685930e782004
voontv <voontv@gmail.com>
2020-04-09 18:05:36 +0700

    add Exercise16

---
+++ NETPROGRAM/BasicExercise/Exercise16.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise16
+    {
+
+        public bool IsPrime(int n)
+        {
+
+            if(n < 2)
+            {
+                return false;
+            }
+
+            for(int i = 2; i <= n / 2; i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public List<int> RangesPrime(int n,int minInRange = 0)
+        {
+
+            return Enumerable.Range(2, n - 1).Where(x => IsPrime(x) && x > minInRange).ToList(); ;
+        }
+
+        public int FindNumber()
+        {
+            var n = 2 * 3 * 5;
+            var minRange = 7;
+            var isCorrect = true;
+
+            do
+            {
+
+                foreach (int i in RangesPrime(n, minRange))
+                {
+                    if(n % i == 0)
+                    {
+                        isCorrect = false;
+                        minRange = n;
+                        n += 2 * 3 * 5;
+                        break;
+                    }
+                }
+            } while (!isCorrect);
+
+            return n;
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise16Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+
+    [TestClass]
+    public class Exercise16Test
+    {
+
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            var test = new Exercise16();
+
+            Assert.AreEqual(test.IsPrime(4), false);
+            Assert.AreEqual(test.IsPrime(145), false);
+            Assert.AreEqual(test.IsPrime(232243), false);
+            Assert.AreEqual(test.IsPrime(13), true);
+            Assert.AreEqual(test.IsPrime(113), true);
+        }
+
+        [TestMethod]
+        public void TestRangersPrime()
+        {
+            var test = new Exercise16();
+
+            Assert.AreEqual(test.RangesPrime(20).Count, 8);
+            Assert.AreEqual(test.RangesPrime(7).Count, 4);
+            Assert.AreEqual(test.RangesPrime(11).Count, 5);
+            Assert.AreEqual(test.RangesPrime(11,9).Count, 1);
+            Assert.AreEqual(test.RangesPrime(11,5).Count, 2);
+        }
+
+        [TestMethod]
+        public void TestFindNumber()
+        {
+            var test = new Exercise16();
+
+            Assert.AreEqual(test.FindNumber(), 30);
+        }
+    }
+}

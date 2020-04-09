ee99ff9fe0eca8d5d44b645fce3a8605e2901581
voontv <voontv@gmail.com>
2020-04-09 11:47:41 +0700

    Add Exercise14

---
+++ NETPROGRAM/BasicExercise/Exercise14.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.BasicExercise
+{
+    public class Exercise14
+    {
+        
+        public int CountNumberZero(int n)
+        {
+
+            return Enumerable.Range(0, n+1).Aggregate((a, b) => {
+                
+                return a += (b % 10 == 0) ? (b.ToString().Length - 1) : ((b % 5 == 0) ? 1 : 0);
+            });
+        }
+    }
+}
---
+++ NETPROGRAMTest/BasicExerciseTest/Exercise14Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.BasicExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.BasicExerciseTest
+{
+    [TestClass]
+    public class Exercise14Test
+    {
+
+        [TestMethod]
+        public void TestCountNumberZero()
+        {
+            var test = new Exercise14();
+
+            Assert.AreEqual(test.CountNumberZero(10), 2);
+            Assert.AreEqual(test.CountNumberZero(4), 0);
+            Assert.AreEqual(test.CountNumberZero(100), 21);
+            Assert.AreEqual(test.CountNumberZero(14), 2);
+            Assert.AreEqual(test.CountNumberZero(15), 3);
+            Assert.AreEqual(test.CountNumberZero(20), 4);        
+        }
+    }
+}

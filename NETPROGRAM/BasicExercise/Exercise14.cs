6aa7ec705aa4e3103521ceff1e1c44cab68718c4
voontv <voontv@gmail.com>
2020-04-09 16:11:04 +0700

    change use sum

--- NETPROGRAM/BasicExercise/Exercise14.cs
+++ NETPROGRAM/BasicExercise/Exercise14.cs
@@ -12,10 +12,7 @@ namespace NETPROGRAM.BasicExercise
         public int CountNumberZero(int n)
         {
 
-            return Enumerable.Range(0, n+1).Aggregate((a, b) => {
-                
-                return a += (b % 10 == 0) ? (b.ToString().Length - 1) : ((b % 5 == 0) ? 1 : 0);
-            });
+            return Enumerable.Range(0, n + 1).Sum(b => (b % 10 == 0) ? (b.ToString().Length - 1) : ((b % 5 == 0) ? 1 : 0));
         }
     }
 }

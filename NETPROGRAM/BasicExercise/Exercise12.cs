ea6bb398a9c2cf91cf6afda31d69ff48e96bbd0e
voontv <voontv@gmail.com>
2020-04-09 08:37:38 +0700

    removes variable redundancy

--- NETPROGRAM/BasicExercise/Exercise12.cs
+++ NETPROGRAM/BasicExercise/Exercise12.cs
@@ -6,16 +6,13 @@ namespace NETPROGRAM.BasicExercise
   
         public bool SymmetryNumber(int n)
         {
-            var length = n.ToString().Length;
             var sum = 0;
-            var i = 1;
             var temp = n;
 
             while(n != 0)
             {
                 sum =10 * sum + (n % 10);
                 n /= 10;
-                i++;
             }
 
             return (sum == temp);

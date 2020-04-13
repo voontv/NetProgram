b056a6ee28b5d497c18ce59262fc857ce8c25378
voontv <voontv@gmail.com>
2020-04-13 10:02:29 +0700

    change my

--- NETPROGRAM/BasicExercise/Exercise14.cs
+++ NETPROGRAM/BasicExercise/Exercise14.cs
@@ -9,28 +9,12 @@ namespace NETPROGRAM.BasicExercise
     public class Exercise14
     {
 
-        public long PowInt(int m, int n)
-        {
-            if (n == 0)
-            {
-                return 1;
-            }
-
-            long multi = 1;
-
-            for (int i = 0; i < n; i++)
-            {
-                multi *= m;
-            }
-
-            return multi;
-        }
-
+      
         public int CountZeroFactorial(int n)
         {
             var count = 0;
 
-            for (int i = 1; i <= n; i++)
+            for (var i = 1; i <= n; i += 5)
             {
 
                 if (i % 5 == 0)

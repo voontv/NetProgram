fc15930af5dbf4c6e1216c878e964d7e756e93be
voontv <voontv@gmail.com>
2020-04-14 13:33:34 +0700

    Add Exercise01

---
+++ NETPROGRAM/ArrayExercise/Exercise01.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise01
+    {
+
+        public List<int> CorrectNumbers(int[] array)
+        {
+            var arrayOrder = (from n in array
+                             orderby n
+                             select n).ToArray();
+            var listNumbers = new List<int>();
+            var count = 1;
+            var i = 0;
+
+            do
+            {
+                if(arrayOrder[i] == arrayOrder[i+1])
+                {
+                    count++;    
+                }
+                else
+                {
+                    count = 1;
+                }
+
+                if(count == 2)
+                {
+                    listNumbers.Add(arrayOrder[i]);
+                }
+
+                i++;
+
+            } while (i < arrayOrder.Length - 1);
+
+            return listNumbers;
+        }
+    }
+}
---
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise01Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.ArrayExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.ArrayExerciseTest
+{
+    
+    [TestClass]
+    public class Exercise01Test
+    {
+
+        [TestMethod]
+        public void TestCorrectNumbers()
+        {
+            var test = new Exercise01();
+
+            Assert.AreEqual(test.CorrectNumbers(new int[] { 1, 2, 4, 5, 6, 8, 9, 3, 2, 4 }).Count, 2);
+            Assert.IsTrue(test.CorrectNumbers(new int[] { 1, 2, 4, 5, 6, 8, 9, 3, 2, 4 }).Contains(2));
+            Assert.IsTrue(test.CorrectNumbers(new int[] { 1, 2, 4, 5, 6, 8, 9, 3, 2, 4 }).Contains(4));
+            Assert.IsFalse(test.CorrectNumbers(new int[] { 1, 2, 4, 5, 6, 8, 9, 3, 2, 4 }).Contains(1));
+        }
+    }
+}

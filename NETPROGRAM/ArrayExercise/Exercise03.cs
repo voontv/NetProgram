c90b11a4fe227218684ef343d61a4d30bb5e4052
voontv <voontv@gmail.com>
2020-04-14 14:19:09 +0700

    Add forder for Array Exercise

---
+++ NETPROGRAM/ArrayExercise/Exercise03.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise03
+    {
+
+        public int SubtractArray(int[] array)
+        {
+
+            return array.Sum(x => (x % 2 == 0) ? x : 0) - array.Sum(x => (x % 2 == 1) ? x : 0);
+        }
+    }
+}

cde2b4491df2b8793f578a6df4e034bc70d4bb4e
voontv <voontv@gmail.com>
2020-04-14 13:53:28 +0700

    Add Exercise02

---
+++ NETPROGRAM/ArrayExercise/Exercise02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise02
+    {
+
+        public int SumMax(int[] array)
+        {
+            var numbers = (from n in array
+                          orderby n descending
+                          select n).ToArray();
+
+            return Enumerable.Range(0,(numbers.Length >= 3) ? 3 : numbers.Length).Sum(x => numbers[x]);
+        }
+    }
+}

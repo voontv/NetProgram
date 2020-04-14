d601aa36d28e00640ac94ad983dea9c61d7b2125
voontv <voontv@gmail.com>
2020-04-14 12:40:07 +0700

    Completed Exercise15- more two day

--- NETPROGRAM/BasicExercise/Exercise15.cs
+++ NETPROGRAM/BasicExercise/Exercise15.cs
@@ -9,108 +9,11 @@ namespace NETPROGRAM.BasicExercise
     public class Exercise15
     {
 
-        public int CountZeroFactorial(int n)
-        {
-            var count = 0;
-
-            for(int i= 1; i <= n; i++)
-            {
-                if(i % 10 == 0)
-                {
-                    for(int j = i.ToString().Length - 1; j >= 0; j--)
-                    {
-                        if(i % PowInt(10, j) == 0)
-                        {
-                            if(i % (5 * PowInt(10, j)) == 0)
-                            {
-                                count += j + 1;
-                            }
-                            else
-                            {
-                                count += j;
-                            }
-                            break;
-                        }
-                    }
-                }
-                else if(i % 5 == 0)
-                {
-                    if((i - 25) % 100 == 0)
-                    {
-                        count += 2;
-                    }
-                    else
-                    {
-                        count += 1;
-                    }
-                }
-            }
-
-            return count;
-        }
-        
-        public long Factorial(int n)
-        {
-
-            return Enumerable.Range(1, n).Aggregate((a, b) => a * b);
-        }
-
-        public long PowInt(int m, int n)
-        {
-            if(n == 0)
-            {
-                return 1;
-            }
-
-            long multi = 1;
-            
-            for(int i = 0; i < n; i++)
-            {
-                multi *= m; 
-            }
-
-            return multi;
-        }
 
         public int CountFactorial(int n)
         {
-            long multiOptimize = 1;
-
-            for(int i = 1; i<= n; i++)
-            {
-                multiOptimize *= i;
-                if (i % 10 == 0)
-                {
-                    for (int j = i.ToString().Length - 1; j >= 0; j--)
-                    {
-                        if (i % PowInt(10, j) == 0)
-                        {
-                            if (i % (5 * PowInt(10, j)) == 0)
-                            {
-                                multiOptimize /= 5 * PowInt(10, j);
-                            }
-                            else
-                            {
-                                multiOptimize /= PowInt(10, j);
-                            }
-                            break;
-                        }
-                    }
-                }
-                else if (i % 5 == 0)
-                {
-                    if ((i - 25) % 100 == 0)
-                    {
-                        multiOptimize /= 100;
-                    }
-                    else
-                    {
-                        multiOptimize /= 10;
-                    }
-                }
-            }
-
-            return multiOptimize.ToString().Length + CountZeroFactorial(n);
+            
+            return (int)Math.Ceiling(Enumerable.Range(1, n).Sum(x =>Math.Log10(x)));
         }
     }
 }
--- NETPROGRAMTest/BasicExerciseTest/Exercise15Test.cs
+++ NETPROGRAMTest/BasicExerciseTest/Exercise15Test.cs
@@ -11,36 +11,6 @@ namespace NETPROGRAMTest.BasicExerciseTest
     [TestClass]
     public class Exercise15Test
     {
-        [TestMethod]
-        public void TestFactorial()
-        {
-            var test = new Exercise15();
-
-            Assert.AreEqual(test.Factorial(4), 24);
-            Assert.AreEqual(test.Factorial(5), 120);
-            Assert.AreEqual(test.Factorial(6), 720);
-        }
-
-        [TestMethod]
-        public void TestPowInt()
-        {
-            var test = new Exercise15();
-
-            Assert.AreEqual(test.PowInt(2, 3), 8);
-            Assert.AreEqual(test.PowInt(4, 4), 256);
-        }
-        [TestMethod]
-        public void TestCountZeroFactorial()
-        {
-            var test = new Exercise15();
-
-            Assert.AreEqual(test.CountZeroFactorial(8), 1);
-            Assert.AreEqual(test.CountZeroFactorial(15), 3);
-            Assert.AreEqual(test.CountZeroFactorial(25), 6);
-            Assert.AreEqual(test.CountZeroFactorial(100), 23);
-            Assert.AreEqual(test.CountZeroFactorial(400), 92);
-            Assert.AreEqual(test.CountZeroFactorial(450), 104);
-        }
 
         [TestMethod]
         public void TestCountFactorial()

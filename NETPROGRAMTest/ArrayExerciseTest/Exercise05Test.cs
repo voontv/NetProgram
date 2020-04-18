06504f17a23c92ddeeeeec77662f99c57769e4de
voontv <voontv@gmail.com>
2020-04-18 19:13:56 +0700

    Add Exercise05

---
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise05Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.ArrayExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.ArrayExerciseTest
+{
+    [TestClass]
+    public class Exercise05Test
+    {
+
+        [TestMethod]
+        public void TestArrayUpMax()
+        {
+            var test = new Exercise05();
+
+            Assert.AreEqual(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 6}).Count, 1);
+            Assert.IsTrue(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 6}).ElementAt(0).Contains(5));
+            Assert.AreEqual(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 4, 6, 1}).Count, 3);
+            Assert.IsTrue(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 4, 6}).ElementAt(0).Contains(2));
+            Assert.IsFalse(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 4, 6}).ElementAt(0).Contains(1));
+            Assert.IsFalse(test.ArrayUpMax(new int[] { 2, 3, 1, 5, 4, 6}).ElementAt(2).Contains(1));
+
+        }
+    }
+}

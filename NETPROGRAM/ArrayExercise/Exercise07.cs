72a31c557e229d756061f90a33711cc659e37569
voontv <voontv@gmail.com>
2020-04-19 20:15:44 +0700

    Add Exercise07

---
+++ NETPROGRAM/ArrayExercise/Exercise07.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise07
+    {
+
+        public List<List<int>> SplitArray(int[] array, int n)
+        {
+            
+            return (from i in Enumerable.Range(1, (int) Math.Ceiling(array.Length * 1.0 / n))
+                   select array.Skip((i - 1) * n).Take( (array.Length - ((i - 1) * n) >= n) ? n : (array.Length - ((i - 1) * n))).ToList()).ToList();
+        }
+    }
+}
---
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise07Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.ArrayExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.ArrayExerciseTest
+{
+    [TestClass]
+    public class Exercise07Test
+    {
+
+        [TestMethod]
+        public void TestSplitArray()
+        {
+            var test = new Exercise07();
+
+            Assert.AreEqual(test.SplitArray(new int[] { 4, 5, 7, 9, 12, 18, 20, 4, 9 }, 2).Count, 5);
+            Assert.AreEqual(test.SplitArray(new int[] { 4, 5, 7, 9, 12, 18, 20, 4, 9 }, 2).ElementAt(4).ElementAt(0), 9);
+            Assert.AreEqual(test.SplitArray(new int[] { 16, 5, 17, 9, 762, 18, 20, 4, 9, 20 }, 3).Count, 4);
+            Assert.IsTrue(test.SplitArray(new int[] { 16, 5, 17, 9, 762, 18, 20, 4, 9, 20 }, 3).ElementAt(1).Contains(762));
+        }
+    }
+}

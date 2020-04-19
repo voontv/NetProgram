3db63f5b20527aa9b9f9a26e29b9728e738f95a4
voontv <voontv@gmail.com>
2020-04-19 19:20:41 +0700

    Add Exercise06

---
+++ NETPROGRAM/ArrayExercise/Exercise06.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise06
+    {
+
+        public int PositionInArray(int[] array, int number)
+        {
+            var left = 0;
+            var right = array.Length - 1;
+            var mid = -1;
+            var arrayToUp = true;
+
+            if (array[right] < array[left])
+            {
+                arrayToUp = false;
+                Array.Reverse(array);
+            }
+
+            while (right != left)
+            {
+                mid = (left + right) / 2;
+
+                if(number == array[mid])
+                {
+                    return arrayToUp ? mid : array.Length - 1 - mid;
+                }
+                else if (number < array[mid])
+                {
+                     right = mid - 1;                    
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+                   
+                if(right == left)
+                {
+                    return (number == array[left]) ? (arrayToUp ? mid : array.Length - 1 - right) : -1; 
+                }
+            }
+            
+           
+            return mid;
+        }
+    }
+}
---
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise06Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NETPROGRAM.ArrayExercise;
+
+namespace NETPROGRAMTest.ArrayExerciseTest
+{
+
+    [TestClass]
+    public class Exercise06Test
+    {
+
+        [TestMethod]
+        public void TestPositionInArray()
+        {
+            var test = new Exercise06();
+
+            Assert.AreEqual(test.PositionInArray(new int[] { 1, 3, 5, 7, 9 }, 8), -1);
+            Assert.AreEqual(test.PositionInArray(new int[] { 1, 3, 5, 7, 9 }, 7), 3);
+            Assert.AreEqual(test.PositionInArray(new int[] { 9, 8, 5, 4, 3, 1, 0 }, 5), 2);
+            Assert.AreEqual(test.PositionInArray(new int[] { 29, 17, 15, 8, 7, 6, 5, 4, 3, 2}, 5), 6);
+        }
+    }
+}

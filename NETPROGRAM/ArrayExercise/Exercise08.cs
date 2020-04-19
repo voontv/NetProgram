7e8ae0f2aab71e968c4d38d95c312ea5faffdf48
voontv <voontv@gmail.com>
2020-04-19 20:29:30 +0700

    Add Exercise08

---
+++ NETPROGRAM/ArrayExercise/Exercise08.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise08
+    {
+
+        public int CountNumbers(int[] array)
+        {
+
+            return (from i in array
+                   where (i % 3 == 0 && i % 5 != 0)
+                   select i).Count();
+        }
+    }
+}

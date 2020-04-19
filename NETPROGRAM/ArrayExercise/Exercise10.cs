2c8ddac0bda97b6dae2291344f89788cdba8a09c
voontv <voontv@gmail.com>
2020-04-19 20:52:53 +0700

    Add Exercise10

---
+++ NETPROGRAM/ArrayExercise/Exercise10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise10
+    {
+
+        public bool IsSquareNumber(int n)
+        {
+
+            for(var i = 1; i<= n; i++)
+            {
+                if(i * i > n)
+                {
+                    return false;
+                }
+
+                if(i * i == n)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int SumSquare(int[] array)
+        {
+
+            return array.Where(IsSquareNumber).Sum();
+        }
+    }
+}

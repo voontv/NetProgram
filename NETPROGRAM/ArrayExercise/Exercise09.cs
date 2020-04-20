eaa49a70dba2da9b8dc3e61e57d7d961d043e2ee
voontv <voontv@gmail.com>
2020-04-20 12:42:07 +0700

    Add Exercise09

---
+++ NETPROGRAM/ArrayExercise/Exercise09.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.ArrayExercise
+{
+    public class Exercise09
+    {
+
+        public int[] CreatedArray(int n)
+        {
+            var random = new Random();
+            var array = new int[n];
+
+            array[0] = random.Next(0, n);
+            for(var i = 1; i < n; i++)
+            {
+                array[i] = random.Next(0, n);
+
+                for (var j = 0; j < i; j++)
+                {
+                    if(array[j] == array[i])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise09Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NETPROGRAM.ArrayExercise;
+
+namespace NETPROGRAMTest.ArrayExerciseTest
+{
+
+    [TestClass]
+    public class Exercise09Test
+    {
+
+        [TestMethod]
+        public void TestCreatedArrray()
+        {
+            var test = new Exercise09();
+
+            Assert.AreEqual(test.CreatedArray(4).Where(x => (x == 3)).Sum(), 3);
+            Assert.IsFalse(test.CreatedArray(4).Contains(4));
+            Assert.IsTrue(test.CreatedArray(4).Contains(3));
+        }
+    }
+}

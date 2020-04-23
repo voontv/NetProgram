632d0579ffde1e4073d479f3b4af7c59839fbe6f
voontv <voontv@gmail.com>
2020-04-23 18:37:22 +0700

    add folder for String exercise

---
+++ NETPROGRAM/StringExercise/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.StringExercise
+{
+    public class Test
+    {
+        public int CountLengthString(String s)
+        {
+
+            return s.Length;
+        }
+    }
+}

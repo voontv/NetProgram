18648a330356dbbc143bb35f85988dfbdfd9743e
voontv <voontv@gmail.com>
2020-04-24 15:02:44 +0700

    Add Exercise01

---
+++ NETPROGRAM/String_Exercise/Exercise01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise01
+    {
+
+        public string SnakeCase(string s)
+        {
+            var snakeString = s.ToLower();
+            for(var i = s.Length - 2 ; i > 0 ; i--)
+            {
+
+                if (s.ToLower()[i] - s[i] > 0)
+                {
+                    snakeString = snakeString.Insert(i, "_");
+                }
+            }
+
+            return snakeString;
+            
+        }
+    }
+}
---
+++ NETPROGRAMTest/String_ExerciseTest/Exercise01Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.String_Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.String_ExerciseTest
+{
+
+    [TestClass]
+    public class Exercise01Test
+    {
+
+        [TestMethod]
+        public void TestSnakeCase()
+        {
+            var test = new Exercise01();
+
+            Assert.AreEqual(test.SnakeCase("myProgram"), "my_program");
+            Assert.AreEqual(test.SnakeCase("myProgramGood"), "my_program_good");
+        }
+        
+    }
+}

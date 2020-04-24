429c2ec8f77d5483cbe3ec3093c77044f4c9dd44
voontv <voontv@gmail.com>
2020-04-24 16:35:44 +0700

    change to return int[][]

--- NETPROGRAM/ArrayExercise/Exercise07.cs
+++ NETPROGRAM/ArrayExercise/Exercise07.cs
@@ -9,10 +9,12 @@ namespace NETPROGRAM.ArrayExercise
     public class Exercise07
     {
 
-        public List<IGrouping<int,int>> SplitArray(int[] array, int n)
+        public int[][] SplitArray(int[] array, int n)
         {
 
-            return (from index in Enumerable.Range(0, array.Length) group array[index] by index / n).ToList();          
+            return (from gr in (from index in Enumerable.Range(0, array.Length) 
+                    group array[index] by (index / n)) 
+                    select gr.ToArray()).ToArray();          
         }
     }
 }

c4ae5972eb1f4b7cc1785d8577dc052c505e040d
voontv <voontv@gmail.com>
2020-04-25 11:57:50 +0700

    Add exercise03

---
+++ NETPROGRAM/String_Exercise/Exercise03.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise03
+    {
+
+        public string ReverseString(string s)
+        {
+
+            return new string(s.Reverse().ToArray());
+        }     
+    }
+}
---
+++ NETPROGRAMTest/String_ExerciseTest/Exercise03Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.String_Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.String_ExerciseTest
+{
+
+    [TestClass]
+    public class Exercise03Test
+    {
+
+        [TestMethod]
+        public void TestReverseString()
+        {
+            var test = new Exercise03();
+
+            Assert.AreEqual(test.ReverseString("Toi la toi"), "iot al ioT");
+        }              
+    }
+}

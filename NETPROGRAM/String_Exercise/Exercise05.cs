9967dbf1158e709fa9a81cfe5380cfdd3d34c8ce
voontv <voontv@gmail.com>
2020-04-25 16:29:48 +0700

    Add exercise05

---
+++ NETPROGRAM/String_Exercise/Exercise05.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise05
+    {
+
+        public bool IsSymmetry(String s)
+        {
+            bool isSymmetry = true;
+
+            for(var i =0; i < s.Length/2; i++)
+            {
+
+                if(s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return isSymmetry;
+        }
+    }
+}

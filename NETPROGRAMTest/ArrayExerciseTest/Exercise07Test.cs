944222041c5baae40285377e39786b6b9201e4d8
voontv <voontv@gmail.com>
2020-04-25 08:44:22 +0700

    learn compare two array use Enumerable.SequenceEqual

--- NETPROGRAMTest/ArrayExerciseTest/Exercise07Test.cs
+++ NETPROGRAMTest/ArrayExerciseTest/Exercise07Test.cs
@@ -20,13 +20,8 @@ namespace NETPROGRAMTest.ArrayExerciseTest
             Assert.AreEqual(test.SplitArray(new int[] { 4, 5, 7, 9, 12, 18, 20, 4, 9 }, 2).Length, 5);
             Assert.AreEqual(test.SplitArray(new int[] { 4, 5, 7, 9, 12, 18, 20, 4, 9 }, 2).ElementAt(4).ElementAt(0), 9);
             Assert.AreEqual(test.SplitArray(new int[] { 16, 5, 17, 9, 762, 18, 20, 4, 9, 20 }, 3).Length, 4);
-            Array.Equals(test.SplitArray(new int[] { 16, 5, 17, 9, 762, 18, 20, 4, 9, 20 }, 3),
-                new[] {
-                    new []{16, 5, 17},
-                    new []{9, 762, 18},
-                    new []{20, 4, 9},
-                    new []{20},
-                });
+            Assert.IsTrue(Enumerable.SequenceEqual(test.SplitArray(new int[] { 16, 5, 17, 9, 762, 18, 20, 4, 9, 20 }, 3)[0],
+                new []{16, 5, 17}));
 
         }
     }

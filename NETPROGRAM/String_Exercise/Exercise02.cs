7edd3dfc401a53db6d5c11369e44821bd9d59b3f
voontv <voontv@gmail.com>
2020-04-25 10:52:59 +0700

    Add exercise02

---
+++ NETPROGRAM/String_Exercise/Exercise02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise02
+    {
+
+        public string UpperCase(string s)
+        {
+            StringBuilder upperString = new StringBuilder("");
+
+            foreach(var subString in s.Split('_'))
+            {
+                upperString.AppendFormat(subString.First().ToString().ToUpper() + subString.Remove(0,1));
+            }
+
+            return upperString.ToString();
+        }
+    }
+}

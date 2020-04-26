2af6b616a722736ac75d7a0a5b4b243f78a02eff
voontv <voontv@gmail.com>
2020-04-26 16:05:11 +0700

    Add Exercise08

---
+++ NETPROGRAM/String_Exercise/Exercise08.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise08
+    {
+
+        public string RemoveCharRedundancy(string s)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(s[0]);
+
+            for (var i = 1; i < s.Length; i++)
+            {
+                if (stringBuilder.ToString().Last() != s[i])
+                {
+                    stringBuilder.Append(s[i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ NETPROGRAMTest/String_ExerciseTest/Exercise08Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETPROGRAM.String_Exercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NETPROGRAMTest.String_ExerciseTest
+{
+
+    [TestClass]
+    public class Exercise08Test
+    {
+
+        [TestMethod]
+        public void TestRemoveCharRedundancy()
+        {
+            var test = new Exercise08();
+
+            Assert.AreEqual(test.RemoveCharRedundancy("abbbbbbccccd eeffffddbc"), "abcd efdbc");
+            Assert.AreEqual(test.RemoveCharRedundancy("abcdee ghhhheeeeeeeeee"), "abcde ghe");
+        }
+    }
+}

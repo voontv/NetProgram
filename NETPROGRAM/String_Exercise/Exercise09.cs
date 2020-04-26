9be4b5d3084f415c5c1a4764df5aed9899bcccfc
voontv <voontv@gmail.com>
2020-04-26 17:21:17 +0700

    Add exercise09

---
+++ NETPROGRAM/String_Exercise/Exercise09.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NETPROGRAM.String_Exercise
+{
+    public class Exercise09
+    {
+
+        public bool IsBarcodeEAN13(string s)
+        {
+
+            return (from index in Enumerable.Range(0, s.Length)
+                    group int.Parse(s[index].ToString()) by index % 2)
+                    .Sum(x => (x.Key == 0) ? x.ToArray().Sum() : (3 * x.ToArray().Sum())) % 10 == 0;
+        }
+    }
+}
